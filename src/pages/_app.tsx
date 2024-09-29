829c6994d14ad05826f097ec69711f47740eb53e
Nguyen Tien Dung <tiendungntd99@gmail.com>
2024-09-29 23:44:31 +0700

    update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,32 @@
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-[#f1ebeb]">
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        limit={2}
-        hideProgressBar
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>Image Uploader | DungNT</title>
+      </Head>
+
+      <div className="bg-[#f1ebeb]">
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          limit={2}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
